b5072fa4151aa665ddbf8b664da71b0867721653
akram20091989 <116184423+akram20091989@users.noreply.github.com>
2023-11-10 14:48:17 +0600

    Initial Commit

---
+++ New Project with AI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New Project with AI
+//
+//  Created by bond on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
